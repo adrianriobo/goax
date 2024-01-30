2e033620a199fe39cdc0833eb659a7abe108cf49
Adrian Riobo Lorenzo <ariobolo@redhat.com>
2024-01-30 09:24:26 +0100

    added type and title as app selector for print operation
        previously the print operation pick the app within the focus to run the print command on it, now if type and title are set as params the app picked is the one from those params
    
    Signed-off-by: Adrian Riobo Lorenzo <ariobolo@redhat.com>

--- cmd/app/print.go
+++ cmd/app/print.go
@@ -25,16 +25,31 @@ func getPrintCmd() *cobra.Command {
 	flagSet := pflag.NewFlagSet("print", pflag.ExitOnError)
 	flagSet.StringP("element", "e", "", "element id/value to be filtered")
 	flagSet.Bool("strict", false, "to force id match exactly with the element id/value")
+	flagSet.StringP("type", "t", "", "type of element. This is OS dependant. For windows it is the type of element (i.e Window) for mac is the bundle id")
+	flagSet.StringP("title", "", "", "title of the main window")
 	c.Flags().AddFlagSet(flagSet)
 
 	return c
 }
 
 func print() error {
-	a, err := app.LoadForefrontApp()
+	var a *app.App
+	var err error
+	aType := viper.GetString("type")
+	aTitle := viper.GetString("title")
+	if len(aType) > 0 && len(aTitle) > 0 {
+		a, err = app.LoadAppByTypeAndTitle(
+			aType,
+			aTitle,
+		)
+	} else {
+		a, err = app.LoadForefrontApp()
+	}
 	if err != nil {
 		return err
 	}
-	a.Print(viper.GetString("element"), viper.IsSet("strict"))
+	a.Print(
+		viper.GetString("element"),
+		viper.IsSet("strict"))
 	return nil
 }
